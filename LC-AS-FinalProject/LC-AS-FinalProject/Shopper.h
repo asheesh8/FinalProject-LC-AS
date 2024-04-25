f5997f6f79adc0e8e59ed687661fd442e0678ec2
asheesh8 <160625210+asheesh8@users.noreply.github.com>
2024-04-25 00:52:02 -0400

    Update Shopper.h
    question... but put parameters on shopper.

--- LC-AS-FinalProject/LC-AS-FinalProject/Shopper.h
+++ LC-AS-FinalProject/LC-AS-FinalProject/Shopper.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include "SystemManager.h"
+#include "SystemManager.h" //inheriting from.
 using namespace std;
 
 class Shopper : public SystemManager
 {
 public:
-	Shopper();
+	Shopper(string username, string password);
 	~Shopper();
 private:
-	
+	//what shopper members do we need?
 };
