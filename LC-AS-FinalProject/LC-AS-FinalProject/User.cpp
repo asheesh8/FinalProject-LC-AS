ffe66d89c2a33b285e76a9f1a9cd868a7542a72c
asheesh8 <160625210+asheesh8@users.noreply.github.com>
2024-04-25 00:39:14 -0400

    Update User.cpp
    initialized members fully. 
    
    kept destructor.

--- LC-AS-FinalProject/LC-AS-FinalProject/User.cpp
+++ LC-AS-FinalProject/LC-AS-FinalProject/User.cpp
@@ -1,11 +1,12 @@
 #include "User.h"
 
 
-User::User(string username1, string password1)
-{
-	
+User::User(string username1, string password1) : password1(pasword1), username1(username1) {}
+string User::getUsername() const {
+    return username1;
 }
 
-User::~User()
-{
+string User::getPassword() const {
+    return password1;
 }
+
